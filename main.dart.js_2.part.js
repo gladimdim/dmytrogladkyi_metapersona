364584652bddf59f61683e5188c89589469e11c6
Dmytro Gladkyi <gladimdim@gmail.com>
2021-11-08 16:35:32 +0200

    Added two Articles
    Deferred loading of Markdown and Share libraries only to times when they are used.

---
+++ main.dart.js_2.part.js
@@ -0,0 +1,9 @@
+self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
+$__dart_deferred_initializers__.current=function(a,b,c,$){var A={
+aDf(d,e){var y=$.aqz
+if(y==null){y=$.auD()
+$.aqz=y}return y.kQ(0,d,null,e)}}
+a.setFunctionNamesIfNecessary([A])
+A=a.updateHolder(c[3],A)
+var z=a.updateTypes([]);(function staticFields(){$.aqz=null})()}
+$__dart_deferred_initializers__["hIC1y7Es6sKECTTFCnk0ietpbBk="] = $__dart_deferred_initializers__.current
