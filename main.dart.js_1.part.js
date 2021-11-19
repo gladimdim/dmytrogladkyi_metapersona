dacf5885ae006822608162be1a455f79c90f1190
Dmytro Gladkyi <gladimdim@gmail.com>
2021-11-19 15:08:39 +0200

    Added Links section for Micros
    Enhanced GridView layout to show more content

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,9 +1,9 @@
 self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
 $__dart_deferred_initializers__.current=function(a,b,c,$){var A={
-aGn(d,e){var y=$.atF
-if(y==null){y=$.axK()
-$.atF=y}return y.lo(0,d,null,e)}}
+aHi(d,e){var y=$.aux
+if(y==null){y=$.ayE()
+$.aux=y}return y.lq(0,d,null,e)}}
 a.setFunctionNamesIfNecessary([A])
 A=a.updateHolder(c[4],A)
-var z=a.updateTypes([]);(function staticFields(){$.atF=null})()}
-$__dart_deferred_initializers__["2nz39Oem9Yf8Iue2ikQKa0Zp6mg="] = $__dart_deferred_initializers__.current
+var z=a.updateTypes([]);(function staticFields(){$.aux=null})()}
+$__dart_deferred_initializers__["ujxKDJwzXu2HFlBnXhzQKLFYBuM="] = $__dart_deferred_initializers__.current
